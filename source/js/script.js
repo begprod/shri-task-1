91302ff96b1e5c43cce98e04424ef624ad5c4ac5
Andrey Aratov <madingame@yandex.ru>
2019-12-14 15:06:05 +0300

    added js

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,23 @@
-console.log('asdasdas');
+const selectors = {
+	themeSwitcherWrapper: '.onoffswitch',
+	themeSwitcherButton: '.onoffswitch__button',
+	historyItem: '.history__transaction',
+	accordionItem: '.e-accordion__more',
+	page: '.page'
+};
+const pageWrapper = document.querySelector(selectors.page);
+const themeSwitcherWrapper = document.querySelector(selectors.themeSwitcherWrapper);
+
+document.addEventListener('click', (event) => {
+	if (event.target.matches(selectors.themeSwitcherButton)) {
+		themeSwitcherWrapper.classList.toggle('onoffswitch_checked');
+		pageWrapper.classList.toggle('theme_color_project-default');
+		pageWrapper.classList.toggle('theme_color_project-inverse');
+	}
+
+	if (event.target.closest(selectors.historyItem)) {
+		const currentItem = event.target.closest(selectors.historyItem);
+
+		currentItem.querySelector(selectors.accordionItem).classList.toggle('history__hide');
+	}
+}, false);
